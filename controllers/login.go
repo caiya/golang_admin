3ac8a5ab6490c7adfa84f4b4f7667778c77a6f0c
晁州 <chaozhou@steelv.cn>
2016-06-12 15:05:51 +0800

    修改登录

---
+++ controllers/login.go
@@ -0,0 +1,30 @@
+// login
+package controllers
+
+import (
+	"fmt"
+)
+
+type LoginController struct {
+	BaseController
+}
+
+func (this *LoginController) UserIndex() {
+	this.TplName = "login.html"
+}
+
+func (this *LoginController) UserLogin() {
+	username := this.GetString("userName")
+	password := this.GetString("password")
+	user, err := CheckLogin(username, password)
+	if err == nil {
+		this.Redirect("/main", 302)
+		user.Pass = ""
+		fmt.Println(user)
+		this.SetSession("userinfo", user)
+	} else {
+		this.Data["user"] = user
+		this.Data["errmsg"] = err.Error()
+		this.TplName = "login.html"
+	}
+}
